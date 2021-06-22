c4b730bc46321289f6a279b94c8bad1e34081457
vietnguyena1 <35227782+vietnguyena1@users.noreply.github.com>
2021-06-22 17:49:57 +0700

    auth added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchResultPage from "./components/search-result/search-result-page";
 import HomePage from "./components/home/home-page";
 import ProductDetailPage from "./components/product-detail/product-detail-page";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch, useHistory } from "react-router-dom";
 import AssignedPostPage from "./components/staff-assigned-post/assigned-post-page";
+import { useAuth, useResolved } from "./hooks";
+import { Login } from "./components/Login";
+import { Signup } from "./components/Signup";
 
-function App() {
-  return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage}></Route>
-          <Route
-            path="/search-result-page/:searchtext"
-            component={SearchResultPage}
-          ></Route>
-          <Route
-            path="/product-detail-page"
-            component={ProductDetailPage}
-          ></Route>
-          <Route
-            path="/assigned-post-page"
-            component={AssignedPostPage}
-          ></Route>
-        </Switch>
-      </Router>
-    </>
+const App = () => {
+  const history = useHistory();
+  const { authUser } = useAuth();
+  const authResolved = useResolved(authUser);
+
+  useEffect(() => {
+    if (authResolved) {
+      history.push(!!authUser ? "/" : "/login");
+    }
+  }, [authResolved, authUser, history]);
+  return authResolved ? (
+    <div className="app">
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route exact path="/" component={HomePage}></Route>
+        <Route
+          path="/search-result-page/:searchtext"
+          component={SearchResultPage}
+        ></Route>
+        <Route
+          path="/product-detail-page"
+          component={ProductDetailPage}
+        ></Route>
+        <Route path="/assigned-post-page" component={AssignedPostPage}></Route>
+      </Switch>
+    </div>
+  ) : (
+    <div>Loading ...</div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "react-datepicker/dist/react-datepicker.css";
-
+import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App style={{fontFamily: "'Roboto', sans-serif"}} /> */}
-    <App
-      style={{
-        fontFamily:
-          "'Haas Grot Text R Web', 'Helvetica Neue', Helvetica, Arial, sans-serif",
-      }}
-    />
+    <Router>
+      {/* <App style={{fontFamily: "'Roboto', sans-serif"}} /> */}
+      <App
+        style={{
+          fontFamily:
+            "'Haas Grot Text R Web', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+        }}
+      />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
