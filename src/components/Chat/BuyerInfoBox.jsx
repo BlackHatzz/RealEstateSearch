44e43c939fe97261be889bbe754cdbb99ad6aeff
vietnguyena1 <35227782+vietnguyena1@users.noreply.github.com>
2021-07-12 17:24:14 +0700

    update seller

--- src/components/Chat/BuyerInfoBox.jsx
+++ src/components/Chat/BuyerInfoBox.jsx
@@ -27,26 +27,37 @@ export const BuyerInfoBox = ({ selectedChat }) => {
 
   return (
     <div className="seller-chat-box-body-right">
-      <p>Điện thoại</p>
-      <p>{chat?.data?.buyerPhone}</p>
-      <p>Thỏa thuận</p>
-      {chat?.data?.deal === undefined && <p>chưa có</p>}
+      <p className="info-title">Điện thoại</p>
+      <p className="info-text">{chat?.data?.buyerPhone}</p>
+      <p className="info-title">Thỏa thuận</p>
+      {chat?.data?.deal === undefined && <p className="info-text">chưa có</p>}
       {chat?.data?.deal === "pending" && (
-        <p>{chat?.data?.dealPrice} tỷ (đang chờ)</p>
+        <p className="info-text">{chat?.data?.dealPrice} tỷ (đang chờ)</p>
       )}
       {chat?.data?.deal === "accepted" && (
-        <p>{chat?.data?.dealPrice} tỷ (đã chấp nhận)</p>
+        <p className="info-text">{chat?.data?.dealPrice} tỷ (đã chấp nhận)</p>
       )}
       {chat?.data?.deal === "refused" && (
         <div>
-          <p>{chat?.data?.dealPrice} tỷ (bạn đã từ chối)</p>
-          <p>Lý do: {chat?.data?.dealReason}</p>
+          <p className="info-text">
+            {chat?.data?.dealPrice} tỷ (bạn đã từ chối)
+          </p>
+          <p className="info-text">Lý do: {chat?.data?.dealReason}</p>
         </div>
       )}
-      <p>Lịch hẹn</p>
-      {chat?.data?.appointment === undefined && <p>chưa có</p>}
+      <p className="info-title">Lịch hẹn</p>
+      {chat?.data?.appointment === undefined && (
+        <p className="info-text">chưa có</p>
+      )}
       {chat?.data?.appointment === "upcoming" && (
-        <p>{moment(chat?.data?.appointmentDate).locale("vi").format("LLLL")}</p>
+        <div>
+          <p className="info-text">
+            {moment(chat?.data?.appointmentDate).locale("vi").format("LLL")}
+          </p>
+          <p className="info-text">
+            {moment(chat?.data?.appointmentDate).fromNow()}
+          </p>
+        </div>
       )}
     </div>
   );
