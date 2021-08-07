8bc1e9836fce896fe1f24c1c3e3a0084d9c8c3af
LouissPham <phamhuuloi987@gmail.com>
2021-08-07 12:51:22 +0700

    update color Chat

--- src/components/Chat/ChatWindowHeader.jsx
+++ src/components/Chat/ChatWindowHeader.jsx
@@ -4,7 +4,7 @@ export const ChatWindowHeader = ({ onClickChat }) => {
   return (
     <div className="chat_window_header">
       <div className="chat_window_header_title">
-        <h2>Chat</h2>
+        <h2 style={{fontSize:"20px"}}>Chat</h2>
       </div>
       <div className="chat_window_close_button" onClick={onClickChat}>
         <KeyboardArrowDownIcon />
